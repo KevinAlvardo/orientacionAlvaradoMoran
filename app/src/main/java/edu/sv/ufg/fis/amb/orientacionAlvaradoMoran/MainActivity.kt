f6d2aa41826b43fad1b120dfd0e43ead0af74860
gerardo <gerardoalvaradox@gmail.com>
2024-04-25 14:39:12 -0600

    orientacion pantalla android

---
+++ app/src/main/java/edu/sv/ufg/fis/amb/orientacionAlvaradoMoran/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.sv.ufg.fis.amb.orientacionAlvaradoMoran
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
